145f8b04cf13a9fc0d2ca6fd37f57a875e967bfd
hochbrj <jhochbrueckner@gmail.com>
2021-07-21 10:42:30 +1000

    fix(config): remove walletconfig type

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,7 @@ import {
   Dapp,
   AddressNetworkMap,
   Network,
-  AddressDappNetworkMap,
-  WalletConfig
+  AddressDappNetworkMap
 } from "./types";
 
 const envFound = dotenv.config();
@@ -17,14 +16,6 @@ if (!envFound) {
 export const factoryAddress: AddressNetworkMap = {
   [Network.MUMBAI]: "0xeec15f2716c7a98fFe14F7aD0dEeC93962aF4437",
   [Network.POLYGON]: "0x72b8CCE4bE8EfDC50e493cB210f9b9Eb15033453"
-};
-
-export const walletConfig: WalletConfig = {
-  privateKey: process.env.PRIVATE_KEY || "",
-  mnemonic: process.env.MNEMONIC || "",
-  accountId: process.env.ACCOUNT_ID || "0",
-  provider: process.env.PROVIDER || "http://localhost:8545",
-  network: (process.env.NETWORK as Network) || Network.MUMBAI
 };
 
 export const routerAddress: AddressDappNetworkMap = {
--- src/entities/utils.test.ts
+++ src/entities/utils.test.ts
@@ -5,11 +5,12 @@ import { Dapp, Network } from "../types";
 
 import { Dhedge } from "./index";
 
-const weth = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619";
+//const weth = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619";
 const usdt = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F";
+const usdc = "0x2791bca1f2de4661ed88a30c99a7a9449aa84174";
 //const lpUsdcWeth = "0x34965ba0ac2451A34a0471F04CCa3F990b8dea27";
 const tradeAmountUsdt = "1000000";
-const tradeAmountWeth = "1000000000000000000";
+//const tradeAmountWeth = "1000000000000000000";
 
 const provider = new ethers.providers.JsonRpcProvider(providerUrl);
 const wallet = new ethers.Wallet(privateKey, provider);
@@ -22,26 +23,38 @@ describe("utils", () => {
     dhedge = new Dhedge(wallet, Network.POLYGON);
   });
 
-  it("calculates lp amount of WETH given 1 USDT to the USDT/WETH pool", async () => {
+  // it("calculates lp amount of WETH given 1 USDT to the USDT/WETH pool", async () => {
+  //   const result = await dhedge.utils.calculateLpAmount(
+  //     Dapp.SUSHISWAP,
+  //     usdt,
+  //     weth,
+  //     tradeAmountUsdt
+  //   );
+  //   expect(Number(result)).toBeLessThan((1 / 800) * 1e18);
+  //   expect(Number(result)).toBeGreaterThan((1 / 5000) * 1e18);
+  // });
+
+  // it("calculates lp amount of USDT given 1 WETH to the USDT/WETH pool", async () => {
+  //   const result = await dhedge.utils.calculateLpAmount(
+  //     Dapp.SUSHISWAP,
+  //     weth,
+  //     usdt,
+  //     tradeAmountWeth
+  //   );
+  //   expect(Number(result)).toBeGreaterThan(800000000); //ETH price 800 USD
+  //   expect(Number(result)).toBeLessThan(5000000000); //ETH price 5000 USD
+  // });
+
+  it("calculates lp amount of USDC given 1 USDT to the USDT/USDC pool", async () => {
     const result = await dhedge.utils.calculateLpAmount(
       Dapp.SUSHISWAP,
       usdt,
-      weth,
+      usdc,
       tradeAmountUsdt
     );
-    expect(Number(result)).toBeLessThan((1 / 800) * 1e18);
-    expect(Number(result)).toBeGreaterThan((1 / 5000) * 1e18);
-  });
-
-  it("calculates lp amount of USDT given 1 WETH to the USDT/WETH pool", async () => {
-    const result = await dhedge.utils.calculateLpAmount(
-      Dapp.SUSHISWAP,
-      weth,
-      usdt,
-      tradeAmountWeth
-    );
-    expect(Number(result)).toBeGreaterThan(800000000); //ETH price 800 USD
-    expect(Number(result)).toBeLessThan(5000000000); //ETH price 5000 USD
+    console.log(result);
+    expect(Number(result)).toBeLessThan(1.2);
+    expect(Number(result)).toBeGreaterThan(0.9);
   });
 
   // it("gets pool Id of sushi LP pool for USDC/WETH pool", async () => {
