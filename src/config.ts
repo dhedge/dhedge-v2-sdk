7b54539364e212163a07a5c896186fc3b4150e10
hochbrj <jhochbrueckner@gmail.com>
2021-09-29 15:06:56 +1000

    feat(quickswap): adds quickswap config

--- src/config.ts
+++ src/config.ts
@@ -15,13 +15,15 @@ export const routerAddress: AddressDappNetworkMap = {
   [Network.POLYGON]: {
     [Dapp.SUSHISWAP]: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
     [Dapp.AAVE]: "0x8dFf5E27EA6b7AC08EbFdf9eB090F32ee9a30fcf",
-    [Dapp.ONEINCH]: "0x11111112542D85B3EF69AE05771c2dCCff4fAa26"
+    [Dapp.ONEINCH]: "0x11111112542D85B3EF69AE05771c2dCCff4fAa26",
+    [Dapp.QUICKSWAP]: "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff"
   }
 };
 
 export const dappFactoryAddress: AddressDappNetworkMap = {
   [Network.POLYGON]: {
-    [Dapp.SUSHISWAP]: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4"
+    [Dapp.SUSHISWAP]: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
+    [Dapp.QUICKSWAP]: "0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32"
   }
 };
 
