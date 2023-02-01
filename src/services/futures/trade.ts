583469055872ed2f5a4eb55ff5536cae010e0ac8
hochbrj <jhochbrueckner@gmail.com>
2023-02-01 10:26:43 -0500

    fix(trade): Change tracking code to 'KWENTA'

--- src/services/futures/trade.ts
+++ src/services/futures/trade.ts
@@ -1,12 +1,12 @@
 import { ethers } from "../..";
-import { PRICE_IMPACT_DELTA } from "./constants";
+import { FUTURES_TRACKING, PRICE_IMPACT_DELTA } from "./constants";
 import { iSynthetixFuturesMarket } from "./market";
 
 export function getFuturesChangePositionTxData(
   amount: ethers.BigNumber | string,
   version: 1 | 2
 ): string {
-  const args = [amount, ethers.utils.formatBytes32String("tracking")];
+  const args = [amount, ethers.utils.formatBytes32String(FUTURES_TRACKING)];
   if (version === 2) args.splice(1, 0, PRICE_IMPACT_DELTA);
   return iSynthetixFuturesMarket(version).encodeFunctionData(
     "modifyPositionWithTracking",
@@ -16,7 +16,7 @@ export function getFuturesChangePositionTxData(
 
 export function getFuturesClosePositionTxData(version: 1 | 2): string {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const args: any[] = [ethers.utils.formatBytes32String("tracking")];
+  const args: any[] = [ethers.utils.formatBytes32String(FUTURES_TRACKING)];
   if (version === 2) args.unshift(PRICE_IMPACT_DELTA);
   return iSynthetixFuturesMarket(version).encodeFunctionData(
     "closePositionWithTracking",
