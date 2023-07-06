d484d7d30e3bef757cc3a93401b9afc9588a7c2d
kent426 <kenthuang426@gmail.com>
2023-07-06 15:09:48 +0800

    refactor(0x): refactored 0x tests

--- src/test/zeroEx.test.ts
+++ src/test/zeroEx.test.ts
@@ -11,174 +11,99 @@ import { getTxOptions } from "./txOptions";
 
 jest.setTimeout(100000);
 
-describe("[Polygon], pool trade via 0x", () => {
-  let network: Network;
-  let dhedge: Dhedge;
-  let pool: Pool;
-  let USDC: string;
-  let WETH: string;
-  let ZeroExProxy: string;
-  beforeAll(async () => {
-    const provider = new ethers.providers.JsonRpcProvider(
-      `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
-    );
-    const wallet = new ethers.Wallet(
-      process.env.PRIVATE_KEY as string,
-      provider
-    );
-    network = Network.POLYGON;
-    USDC = CONTRACT_ADDRESS[network].USDC;
-    WETH = CONTRACT_ADDRESS[network].WETH;
+const launch0xTests = (
+  network: Network,
+  provider: ethers.providers.JsonRpcProvider
+) => {
+  describe(`[${network}], pool trade via 0x`, () => {
+    let dhedge: Dhedge;
+    let pool: Pool;
+    let USDC: string;
+    let WETH: string;
+    let ZeroExProxy: string;
+    beforeAll(async () => {
+      const wallet = new ethers.Wallet(
+        process.env.PRIVATE_KEY as string,
+        provider
+      );
+      USDC = CONTRACT_ADDRESS[network].USDC;
+      WETH = CONTRACT_ADDRESS[network].WETH;
 
-    ZeroExProxy = routerAddress[network][Dapp.ZEROEX]!;
-    dhedge = new Dhedge(wallet, network);
-    pool = await dhedge.loadPool(TEST_POOL_FOR_0X[network]);
-  });
+      ZeroExProxy = routerAddress[network][Dapp.ZEROEX]!;
+      dhedge = new Dhedge(wallet, network);
+      pool = await dhedge.loadPool(TEST_POOL_FOR_0X[network]);
+    });
 
-  it("[Polygon] approves unlimited USDC on 0x", async () => {
-    const assetContract = new ethers.Contract(USDC, IERC20.abi, pool.signer);
-    const allowanceBefore = await assetContract.allowance(
-      pool.address,
-      ZeroExProxy
-    );
-    if (ethers.constants.Zero.eq(allowanceBefore)) {
+    it(`[${network}] approves unlimited USDC on 0x`, async () => {
+      const assetContract = new ethers.Contract(USDC, IERC20.abi, pool.signer);
+      const allowanceBefore = await assetContract.allowance(
+        pool.address,
+        ZeroExProxy
+      );
+      if (ethers.constants.Zero.eq(allowanceBefore)) {
+        const options = await getTxOptions(network);
+        const approveTx = await pool.approve(
+          Dapp.ZEROEX,
+          USDC,
+          MAX_AMOUNT,
+          options
+        );
+        await approveTx.wait(1);
+      }
+      const allowanceAfter = await assetContract.allowance(
+        pool.address,
+        ZeroExProxy
+      );
+      await expect(allowanceAfter.gt(0));
+    });
+
+    it(`[${network}] trades 1 USDC into WETH on 0x,  with amountIn as string`, async () => {
       const options = await getTxOptions(network);
-      const approveTx = await pool.approve(
+      const tradeTx = await pool.trade(
         Dapp.ZEROEX,
         USDC,
-        MAX_AMOUNT,
+        WETH,
+        "1000000",
+        0.5,
         options
       );
-      await approveTx.wait(1);
-    }
-    const allowanceAfter = await assetContract.allowance(
-      pool.address,
-      ZeroExProxy
-    );
-    await expect(allowanceAfter.gt(0));
-  });
+      await tradeTx.wait(1);
+      const wethBalanceDelta = await balanceDelta(
+        pool.address,
+        WETH,
+        pool.signer
+      );
+      expect(wethBalanceDelta.gt(0));
+    });
 
-  it("[Polygon] trades 1 USDC into WETH on 0x,  with amountIn as string", async () => {
-    const options = await getTxOptions(network);
-    const tradeTx = await pool.trade(
-      Dapp.ZEROEX,
-      USDC,
-      WETH,
-      "1000000",
-      0.5,
-      options
-    );
-    await tradeTx.wait(1);
-    const wethBalanceDelta = await balanceDelta(
-      pool.address,
-      WETH,
-      pool.signer
-    );
-    expect(wethBalanceDelta.gt(0));
-  });
-
-  it("[Polygon] trades 1 USDC into WETH on 0x, with amountIn as BigNumber", async () => {
-    const options = await getTxOptions(network);
-    const tradeTx = await pool.trade(
-      Dapp.ZEROEX,
-      USDC,
-      WETH,
-      ethers.BigNumber.from(1000000),
-      0.5,
-      options
-    );
-    await tradeTx.wait(1);
-    const wethBalanceDelta = await balanceDelta(
-      pool.address,
-      WETH,
-      pool.signer
-    );
-    expect(wethBalanceDelta.gt(0));
-  });
-});
-
-describe("[Optimism], pool trade via 0x", () => {
-  let network: Network;
-  let dhedge: Dhedge;
-  let pool: Pool;
-  let USDC: string;
-  let WETH: string;
-  let ZeroExProxy: string;
-  beforeAll(async () => {
-    const provider = new ethers.providers.JsonRpcProvider(
-      `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
-    );
-    const wallet = new ethers.Wallet(
-      process.env.PRIVATE_KEY as string,
-      provider
-    );
-    network = Network.OPTIMISM;
-    USDC = CONTRACT_ADDRESS[network].USDC;
-    WETH = CONTRACT_ADDRESS[network].WETH;
-
-    ZeroExProxy = routerAddress[network][Dapp.ZEROEX]!;
-    dhedge = new Dhedge(wallet, network);
-    pool = await dhedge.loadPool(TEST_POOL_FOR_0X[network]);
-  });
-
-  it("[Optimism] approves unlimited USDC on 0x", async () => {
-    const assetContract = new ethers.Contract(USDC, IERC20.abi, pool.signer);
-    const allowanceBefore = await assetContract.allowance(
-      pool.address,
-      ZeroExProxy
-    );
-    if (ethers.constants.Zero.eq(allowanceBefore)) {
+    it(`[${network}] trades 1 USDC into WETH on 0x, with amountIn as BigNumber`, async () => {
       const options = await getTxOptions(network);
-      const approveTx = await pool.approve(
+      const tradeTx = await pool.trade(
         Dapp.ZEROEX,
         USDC,
-        MAX_AMOUNT,
+        WETH,
+        ethers.BigNumber.from(1000000),
+        0.5,
         options
       );
-      await approveTx.wait(1);
-    }
-    const allowanceAfter = await assetContract.allowance(
-      pool.address,
-      ZeroExProxy
-    );
-    await expect(allowanceAfter.gt(0));
+      await tradeTx.wait(1);
+      const wethBalanceDelta = await balanceDelta(
+        pool.address,
+        WETH,
+        pool.signer
+      );
+      expect(wethBalanceDelta.gt(0));
+    });
   });
+};
 
-  it("[Optimism] trades 1 USDC into WETH on 0x,  with amountIn as string", async () => {
-    const options = await getTxOptions(network);
-    const tradeTx = await pool.trade(
-      Dapp.ZEROEX,
-      USDC,
-      WETH,
-      "1000000",
-      0.5,
-      options
-    );
-    await tradeTx.wait(1);
-    const wethBalanceDelta = await balanceDelta(
-      pool.address,
-      WETH,
-      pool.signer
-    );
-    expect(wethBalanceDelta.gt(0));
-  });
+const polygonProvider = new ethers.providers.JsonRpcProvider(
+  `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+);
 
-  it("[Optimism] trades 1 USDC into WETH on 0x, with amountIn as BigNumber", async () => {
-    const options = await getTxOptions(network);
-    const tradeTx = await pool.trade(
-      Dapp.ZEROEX,
-      USDC,
-      WETH,
-      ethers.BigNumber.from(1000000),
-      0.5,
-      options
-    );
-    await tradeTx.wait(1);
-    const wethBalanceDelta = await balanceDelta(
-      pool.address,
-      WETH,
-      pool.signer
-    );
-    expect(wethBalanceDelta.gt(0));
-  });
-});
+const optimismProvider = new ethers.providers.JsonRpcProvider(
+  `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+);
+
+launch0xTests(Network.POLYGON, polygonProvider);
+launch0xTests(Network.OPTIMISM, optimismProvider);
