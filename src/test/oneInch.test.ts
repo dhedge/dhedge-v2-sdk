d78911f082606b4c1c26217a50b8e2a704a7b34b
Kent Huang <kent426@users.noreply.github.com>
2024-06-25 19:30:01 +0800

    fix(hardhat-version): upgraded hardhat version and fixed the forked issue in 1inch tests (#59)

--- src/test/oneInch.test.ts
+++ src/test/oneInch.test.ts
@@ -3,13 +3,20 @@
 import { Dhedge, Pool } from "..";
 
 import { Dapp, Network } from "../types";
-import { CONTRACT_ADDRESS, TEST_POOL } from "./constants";
-import { TestingRunParams, testingHelper, wait } from "./utils/testingHelper";
-import { balanceDelta } from "./utils/token";
+import { CONTRACT_ADDRESS, MAX_AMOUNT, TEST_POOL } from "./constants";
+import {
+  TestingRunParams,
+  setUSDCAmount,
+  testingHelper,
+  wait
+} from "./utils/testingHelper";
+import { allowanceDelta, balanceDelta } from "./utils/token";
 import { getTxOptions } from "./txOptions";
+import BigNumber from "bignumber.js";
+import { routerAddress } from "../config";
 // import { routerAddress } from "../config";
 
-const testOneInch = ({ wallet, network }: TestingRunParams) => {
+const testOneInch = ({ wallet, network, provider }: TestingRunParams) => {
   const USDC = CONTRACT_ADDRESS[network].USDC;
   const WETH = CONTRACT_ADDRESS[network].WETH;
 
@@ -22,60 +29,60 @@ const testOneInch = ({ wallet, network }: TestingRunParams) => {
       dhedge = new Dhedge(wallet, network);
       pool = await dhedge.loadPool(TEST_POOL[network]);
       // top up gas
-      // await provider.send("hardhat_setBalance", [
-      //   wallet.address,
-      //   "0x10000000000000000"
-      // ]);
-      // await provider.send("evm_mine", []);
-      // // top up USDC
-      // await setUSDCAmount({
-      //   amount: new BigNumber(2).times(1e6).toFixed(0),
-      //   userAddress: pool.address,
-      //   network,
-      //   provider
-      // });
+      await provider.send("hardhat_setBalance", [
+        wallet.address,
+        "0x10000000000000000"
+      ]);
+      await provider.send("evm_mine", []);
+      // top up USDC
+      await setUSDCAmount({
+        amount: new BigNumber(2).times(1e6).toFixed(0),
+        userAddress: pool.address,
+        network,
+        provider
+      });
     });
 
-    // it("approves unlimited USDC on 1Inch", async () => {
-    //   await pool.approve(Dapp.ONEINCH, USDC, MAX_AMOUNT);
-    //   const usdcAllowanceDelta = await allowanceDelta(
-    //     pool.address,
-    //     USDC,
-    //     routerAddress[network]["1inch"]!,
-    //     pool.signer
-    //   );
-    //   await expect(usdcAllowanceDelta.gt(0));
-    // });
+    it("approves unlimited USDC on 1Inch", async () => {
+      await pool.approve(Dapp.ONEINCH, USDC, MAX_AMOUNT);
+      const usdcAllowanceDelta = await allowanceDelta(
+        pool.address,
+        USDC,
+        routerAddress[network]["1inch"]!,
+        pool.signer
+      );
+      await expect(usdcAllowanceDelta.gt(0));
+    });
 
-    // it("gets gas estimation for 2 USDC into WETH on 1Inch", async () => {
-    //   const gasEstimate = await pool.trade(
-    //     Dapp.ONEINCH,
-    //     USDC,
-    //     WETH,
-    //     "2000000",
-    //     1,
-    //     await getTxOptions(network),
-    //     true
-    //   );
-    //   expect(gasEstimate.gt(0));
-    // });
+    it("gets gas estimation for 2 USDC into WETH on 1Inch", async () => {
+      const gasEstimate = await pool.trade(
+        Dapp.ONEINCH,
+        USDC,
+        WETH,
+        "2000000",
+        1,
+        await getTxOptions(network),
+        true
+      );
+      expect(gasEstimate.gt(0));
+    });
 
-    // it("gets error on gas estimation for 200 USDC into WETH on 1Inch", async () => {
-    //   await wait(1);
-    //   let gasEstimate = null;
-    //   try {
-    //     gasEstimate = await pool.trade(
-    //       Dapp.ONEINCH,
-    //       USDC,
-    //       WETH,
-    //       "200000000",
-    //       1,
-    //       await getTxOptions(network),
-    //       true
-    //     );
-    //   } catch (err) {}
-    //   expect(gasEstimate).toBeNull();
-    // });
+    it("gets error on gas estimation for 200 USDC into WETH on 1Inch", async () => {
+      await wait(1);
+      let gasEstimate = null;
+      try {
+        gasEstimate = await pool.trade(
+          Dapp.ONEINCH,
+          USDC,
+          WETH,
+          "200000000",
+          1,
+          await getTxOptions(network),
+          true
+        );
+      } catch (err) {}
+      expect(gasEstimate).toBeNull();
+    });
 
     it("trades 2 USDC into WETH on 1Inch", async () => {
       await wait(1);
@@ -99,7 +106,6 @@ const testOneInch = ({ wallet, network }: TestingRunParams) => {
 
 testingHelper({
   network: Network.OPTIMISM,
-  onFork: false,
   testingRun: testOneInch
 });
 
