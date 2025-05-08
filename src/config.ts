66e904476621026966fac5f3a3c03f47229dd23b
hochbrj <jhochbrueckner@gmail.com>
2025-05-08 12:47:04 +0200

    fix(getRewards): Simplify reward claiming

--- src/config.ts
+++ src/config.ts
@@ -97,9 +97,7 @@ export const stakingAddress: AddressDappNetworkMap = {
   [Network.BASE]: {
     [Dapp.PANCAKECL]: "0xC6A2Db661D5a5690172d8eB0a7DEA2d3008665A3"
   },
-  [Network.SONIC]: {
-    [Dapp.SHADOWCL]: "0xe879d0E44e6873cf4ab71686055a4f6817685f02"
-  }
+  [Network.SONIC]: {}
 };
 
 export const aaveAddressProvider: AddressDappNetworkMap = {
