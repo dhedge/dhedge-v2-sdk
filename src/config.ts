fe4bb78bdb657983df6a6348c050c8cc67134f0c
hochbrj <jhochbrueckner@gmail.com>
2025-05-14 13:57:02 +0200

    chore(config): Add comment

--- src/config.ts
+++ src/config.ts
@@ -98,7 +98,7 @@ export const stakingAddress: AddressDappNetworkMap = {
     [Dapp.PANCAKECL]: "0xC6A2Db661D5a5690172d8eB0a7DEA2d3008665A3"
   },
   [Network.SONIC]: {
-    [Dapp.SHADOWCL]: "0xe879d0E44e6873cf4ab71686055a4f6817685f02"
+    [Dapp.SHADOWCL]: "0xe879d0E44e6873cf4ab71686055a4f6817685f02" //specific  for wS/USDC CL
   }
 };
 
