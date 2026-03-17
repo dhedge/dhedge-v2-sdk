8249ae634012e5d0e11594221f608d06f0c65357
hochbrj <jhochbrueckner@gmail.com>
2026-03-17 14:32:32 +0100

    chore(testTorosLimitOrder): Uncomment test cases

--- src/test/torosLimitOrder.test.ts
+++ src/test/torosLimitOrder.test.ts
@@ -28,81 +28,81 @@ const testTorosLimitOrder = ({ wallet, network }: TestingRunParams) => {
     beforeAll(async () => {
       dhedge = new Dhedge(wallet, network);
       pool = await dhedge.loadPool(
-        "0xdd3d575fae102a12aaa086d97c7a8814eff42ebc"
+        "0xdd3d575fae102a12aaa086d97c7a8814eff42ebc" // MCP test pool (with GOLDBULL2X)
       );
     });
 
-    // it("approves Toros vault token for LimitOrderManager", async () => {
-    //   const tx = await pool.approveTorosLimitOrder(
-    //     TOROS_GOLDBULL2X,
-    //     MAX_AMOUNT
-    //   );
-    //   await tx.wait(1);
-    //   const delta = await allowanceDelta(
-    //     pool.address,
-    //     TOROS_GOLDBULL2X,
-    //     limitOrderAddress[network],
-    //     pool.signer
-    //   );
-    //   expect(delta.gt(0)).toBe(true);
-    // });
+    it("approves Toros vault token for LimitOrderManager", async () => {
+      const tx = await pool.approveTorosLimitOrder(
+        TOROS_GOLDBULL2X,
+        MAX_AMOUNT
+      );
+      await tx.wait(1);
+      const delta = await allowanceDelta(
+        pool.address,
+        TOROS_GOLDBULL2X,
+        limitOrderAddress[network],
+        pool.signer
+      );
+      expect(delta.gt(0)).toBe(true);
+    });
 
-    // it("creates a limit order", async () => {
-    //   const tx = await pool.createTorosLimitOrder(
-    //     TOROS_GOLDBULL2X,
-    //     ORDER_AMOUNT,
-    //     STOP_LOSS,
-    //     TAKE_PROFIT,
-    //     GOLD_PRICING_ASSET
-    //   );
-    //   await tx.wait(1);
-    //   const order = await pool.getTorosLimitOrder(
-    //     pool.address,
-    //     TOROS_GOLDBULL2X
-    //   );
-    //   expect(order).not.toBeNull();
-    //   expect(order!.amount.eq(ORDER_AMOUNT)).toBe(true);
-    //   expect(order!.stopLossPriceD18.eq(STOP_LOSS)).toBe(true);
-    //   expect(order!.takeProfitPriceD18.eq(TAKE_PROFIT)).toBe(true);
-    //   expect(order!.pool.toLowerCase()).toBe(TOROS_GOLDBULL2X.toLowerCase());
-    //   expect(order!.pricingAsset.toLowerCase()).toBe(
-    //     GOLD_PRICING_ASSET.toLowerCase()
-    //   );
-    // });
+    it("creates a limit order", async () => {
+      const tx = await pool.createTorosLimitOrder(
+        TOROS_GOLDBULL2X,
+        ORDER_AMOUNT,
+        STOP_LOSS,
+        TAKE_PROFIT,
+        GOLD_PRICING_ASSET
+      );
+      await tx.wait(1);
+      const order = await pool.getTorosLimitOrder(
+        pool.address,
+        TOROS_GOLDBULL2X
+      );
+      expect(order).not.toBeNull();
+      expect(order!.amount.eq(ORDER_AMOUNT)).toBe(true);
+      expect(order!.stopLossPriceD18.eq(STOP_LOSS)).toBe(true);
+      expect(order!.takeProfitPriceD18.eq(TAKE_PROFIT)).toBe(true);
+      expect(order!.pool.toLowerCase()).toBe(TOROS_GOLDBULL2X.toLowerCase());
+      expect(order!.pricingAsset.toLowerCase()).toBe(
+        GOLD_PRICING_ASSET.toLowerCase()
+      );
+    });
 
-    // it("modifies a toros limit order", async () => {
-    //   const order = await pool.getTorosLimitOrder(
-    //     pool.address,
-    //     TOROS_GOLDBULL2X
-    //   );
-    //   if (!order) throw new Error("No existing order found");
+    it("modifies a toros limit order", async () => {
+      const order = await pool.getTorosLimitOrder(
+        pool.address,
+        TOROS_GOLDBULL2X
+      );
+      if (!order) throw new Error("No existing order found");
 
-    //   const newStopLoss = order.stopLossPriceD18
-    //     .mul(90)
-    //     .div(100)
-    //     .toString();
-    //   const newTakeProfit = order.takeProfitPriceD18
-    //     .mul(110)
-    //     .div(100)
-    //     .toString();
+      const newStopLoss = order.stopLossPriceD18
+        .mul(90)
+        .div(100)
+        .toString();
+      const newTakeProfit = order.takeProfitPriceD18
+        .mul(110)
+        .div(100)
+        .toString();
 
-    //   const tx = await pool.modifyTorosLimitOrder(
-    //     TOROS_GOLDBULL2X,
-    //     order.amount,
-    //     newStopLoss,
-    //     newTakeProfit,
-    //     order.pricingAsset
-    //   );
-    //   await tx.wait(1);
+      const tx = await pool.modifyTorosLimitOrder(
+        TOROS_GOLDBULL2X,
+        order.amount,
+        newStopLoss,
+        newTakeProfit,
+        order.pricingAsset
+      );
+      await tx.wait(1);
 
-    //   const modifiedOrder = await pool.getTorosLimitOrder(
-    //     pool.address,
-    //     TOROS_GOLDBULL2X
-    //   );
-    //   expect(modifiedOrder).not.toBeNull();
-    //   expect(modifiedOrder!.stopLossPriceD18.toString()).toBe(newStopLoss);
-    //   expect(modifiedOrder!.takeProfitPriceD18.toString()).toBe(newTakeProfit);
-    // });
+      const modifiedOrder = await pool.getTorosLimitOrder(
+        pool.address,
+        TOROS_GOLDBULL2X
+      );
+      expect(modifiedOrder).not.toBeNull();
+      expect(modifiedOrder!.stopLossPriceD18.toString()).toBe(newStopLoss);
+      expect(modifiedOrder!.takeProfitPriceD18.toString()).toBe(newTakeProfit);
+    });
 
     it("deletes a toros limit order", async () => {
       const order = await pool.getTorosLimitOrder(
