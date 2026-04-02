0c9bdffabeb7217382b00b47b8c42d9daf031683
kent426 <kenthuang426@gmail.com>
2026-04-02 10:02:19 +0800

    test(toros): enabled toros tests with impersonation and oracle staleness fixes

--- src/test/toros.test.ts
+++ src/test/toros.test.ts
@@ -1,21 +1,24 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
 import BigNumber from "bignumber.js";
+import { ethers } from "ethers";
 import { Dhedge, Pool } from "..";
-import { routerAddress } from "../config";
 
 import { Dapp, Network } from "../types";
-import { CONTRACT_ADDRESS, MAX_AMOUNT, TEST_POOL } from "./constants";
+import { CONTRACT_ADDRESS, MAX_AMOUNT } from "./constants";
 import {
+  fixOracleAggregatorStaleness,
   setChainlinkTimeout,
   setUSDCAmount,
   testingHelper,
   TestingRunParams
 } from "./utils/testingHelper";
-
 import { allowanceDelta, balanceDelta } from "./utils/token";
+import { routerAddress } from "../config";
+import PoolLogic from "../abi/PoolLogic.json";
+import PoolManagerLogic from "../abi/PoolManagerLogic.json";
 
-const testToros = ({ wallet, network, provider }: TestingRunParams) => {
+const testToros = ({ network, provider }: TestingRunParams) => {
   const USDC = CONTRACT_ADDRESS[network].USDC;
   const TOROS = CONTRACT_ADDRESS[network].TOROS;
 
@@ -25,15 +28,41 @@ const testToros = ({ wallet, network, provider }: TestingRunParams) => {
 
   describe(`pool on ${network}`, () => {
     beforeAll(async () => {
-      dhedge = new Dhedge(wallet, network);
-      pool = await dhedge.loadPool(TEST_POOL[network]);
-      await setChainlinkTimeout({ pool, provider }, 86400 * 365);
-      // top up gas
+      const poolAddress = "0x2d4cddd2c4fa854536593bcf61d0da3b63ed80cb";
+
+      // Get the pool's manager address directly from the contracts
+      const poolLogic = new ethers.Contract(
+        poolAddress,
+        PoolLogic.abi,
+        provider
+      );
+      const managerLogicAddress: string = await poolLogic.poolManagerLogic();
+      const managerLogic = new ethers.Contract(
+        managerLogicAddress,
+        PoolManagerLogic.abi,
+        provider
+      );
+      const managerAddress: string = await managerLogic.manager();
+
+      // Impersonate the pool's manager and fund with ETH for gas
+      await provider.send("hardhat_impersonateAccount", [managerAddress]);
       await provider.send("hardhat_setBalance", [
-        wallet.address,
-        "0x10000000000000000"
+        managerAddress,
+        ethers.utils.hexValue(ethers.utils.parseEther("100"))
       ]);
-      await provider.send("evm_mine", []);
+
+      // Load pool with the impersonated manager signer
+      const managerSigner = provider.getSigner(managerAddress);
+      dhedge = new Dhedge((managerSigner as unknown) as ethers.Wallet, network);
+      pool = await dhedge.loadPool(poolAddress, false);
+
+      await setChainlinkTimeout({ pool, provider }, 86400 * 365);
+      await fixOracleAggregatorStaleness({ pool, provider });
+
+      // Also fix the Toros vault's aggregators (in case it has different assets)
+      const torosPool = await dhedge.loadPool(TOROS);
+      await fixOracleAggregatorStaleness({ pool: torosPool, provider });
+
       // top up USDC
       const amount = new BigNumber(100).times(1e6).toFixed(0);
       await setUSDCAmount({
@@ -66,6 +95,37 @@ const testToros = ({ wallet, network, provider }: TestingRunParams) => {
       expect(torosBalanceDelta.gt(0));
     });
 
+    it("get Tx data for init and complete withdrawal", async () => {
+      // await provider.send("evm_increaseTime", [86400]);
+      // await provider.send("evm_mine", []);
+      const torosBalance = await pool.utils.getBalance(TOROS, pool.address);
+      await pool.approve(Dapp.TOROS, TOROS, MAX_AMOUNT);
+      const tradeResult = await pool.trade(
+        Dapp.TOROS,
+        TOROS,
+        USDC,
+        torosBalance,
+        1.5,
+        null,
+        {
+          estimateGas: false,
+          onlyGetTxData: true
+        }
+      );
+      expect(tradeResult.minAmountOut).toBeDefined();
+      const completWithdrawResult = await pool.completeTorosWithdrawal(
+        USDC,
+        5,
+        null,
+        {
+          estimateGas: false,
+          onlyGetTxData: true
+        },
+        tradeResult.minAmountOut
+      );
+      expect(completWithdrawResult.txData).toBeDefined();
+    });
+
     it("init Toros Token for withdrawal", async () => {
       await provider.send("evm_increaseTime", [86400]);
       await provider.send("evm_mine", []);
@@ -81,7 +141,7 @@ const testToros = ({ wallet, network, provider }: TestingRunParams) => {
     });
 
     it("complete withdrawal from Toros asset", async () => {
-      await pool.completeTorosWithdrawal(USDC, 1.5);
+      await pool.completeTorosWithdrawal(USDC, 5);
       const usdcBalanceDelta = await balanceDelta(
         pool.address,
         USDC,
@@ -93,7 +153,7 @@ const testToros = ({ wallet, network, provider }: TestingRunParams) => {
 };
 
 testingHelper({
-  network: Network.OPTIMISM,
+  network: Network.ARBITRUM,
   testingRun: testToros,
   onFork: true
 });
