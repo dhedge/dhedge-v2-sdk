d5319f7cf08ae04bcb6fdd70c47e751914741799
kent426 <kenthuang426@gmail.com>
2026-04-23 10:14:34 +0800

    test(aave): fixed aave test with fresh pool, WBTC borrow, and proper assertions

--- src/test/aave.test.ts
+++ src/test/aave.test.ts
@@ -1,87 +1,116 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
+import BigNumber from "bignumber.js";
 import { Dhedge, Pool } from "..";
 import { routerAddress } from "../config";
 import { Dapp, Network } from "../types";
-import { CONTRACT_ADDRESS, MAX_AMOUNT, TEST_POOL } from "./constants";
+import { CONTRACT_ADDRESS, MAX_AMOUNT } from "./constants";
+import {
+  TestingRunParams,
+  fixOracleAggregatorStaleness,
+  setChainlinkTimeout,
+  setUSDCAmount,
+  testingHelper
+} from "./utils/testingHelper";
 import { allowanceDelta, balanceDelta } from "./utils/token";
 
-import { wallet } from "./wallet";
+const testAave = ({ wallet, network, provider }: TestingRunParams) => {
+  const USDC = CONTRACT_ADDRESS[network].USDC;
+  const WBTC = CONTRACT_ADDRESS[network].WBTC;
 
-// const network = Network.OPTIMISM;
-// const network = Network.POLYGON;
-const network = Network.ARBITRUM;
-const USDC = CONTRACT_ADDRESS[network].USDC;
-const WETH = CONTRACT_ADDRESS[network].WETH;
+  let dhedge: Dhedge;
+  let pool: Pool;
+  jest.setTimeout(100000);
 
-let dhedge: Dhedge;
-let pool: Pool;
-jest.setTimeout(100000);
+  describe(`[${network}] aave v3 tests`, () => {
+    beforeAll(async () => {
+      await provider.send("hardhat_setBalance", [
+        wallet.address,
+        "0x10000000000000000"
+      ]);
+      dhedge = new Dhedge(wallet, network);
 
-describe("pool", () => {
-  beforeAll(async () => {
-    dhedge = new Dhedge(wallet, network);
-    pool = await dhedge.loadPool(TEST_POOL[network]);
+      pool = await dhedge.createPool("Test Manager", "Aave Test", "AT", [
+        [USDC, true],
+        [WBTC, false],
+        [routerAddress[network][Dapp.AAVEV3]!, false]
+      ]);
+
+      await setChainlinkTimeout({ pool, provider }, 86400 * 365);
+      await fixOracleAggregatorStaleness({ pool, provider });
+
+      await setUSDCAmount({
+        amount: new BigNumber(1000).times(1e6).toFixed(0),
+        userAddress: pool.address,
+        network,
+        provider
+      });
+    });
+
+    it("approves unlimited USDC on Aave Lending pool", async () => {
+      await pool.approve(Dapp.AAVEV3, USDC, MAX_AMOUNT);
+      const usdcAllowanceDelta = await allowanceDelta(
+        pool.address,
+        USDC,
+        routerAddress[network][Dapp.AAVEV3]!,
+        pool.signer
+      );
+      expect(usdcAllowanceDelta.gt(0)).toBe(true);
+    });
+
+    it("approves unlimited WBTC on Aave Lending pool", async () => {
+      await pool.approve(Dapp.AAVEV3, WBTC, MAX_AMOUNT);
+      const wbtcAllowanceDelta = await allowanceDelta(
+        pool.address,
+        WBTC,
+        routerAddress[network][Dapp.AAVEV3]!,
+        pool.signer
+      );
+      expect(wbtcAllowanceDelta.gt(0)).toBe(true);
+    });
+
+    it("lends 500 USDC into Aave lending pool", async () => {
+      await pool.lend(Dapp.AAVEV3, USDC, "500000000");
+      const usdcBalanceDelta = await balanceDelta(
+        pool.address,
+        USDC,
+        pool.signer
+      );
+      expect(usdcBalanceDelta.eq("-500000000")).toBe(true);
+    });
+
+    it("borrows 0.0001 WBTC from Aave lending pool", async () => {
+      await pool.borrow(Dapp.AAVEV3, WBTC, "10000");
+      const wbtcBalanceDelta = await balanceDelta(
+        pool.address,
+        WBTC,
+        pool.signer
+      );
+      expect(wbtcBalanceDelta.eq("10000")).toBe(true);
+    });
+
+    it("repays 0.0001 WBTC to Aave lending pool", async () => {
+      await pool.repay(Dapp.AAVEV3, WBTC, "10000");
+      const wbtcBalanceDelta = await balanceDelta(
+        pool.address,
+        WBTC,
+        pool.signer
+      );
+      expect(wbtcBalanceDelta.eq("-10000")).toBe(true);
+    });
+
+    it("withdraws 400 USDC from Aave lending pool", async () => {
+      await pool.withdrawDeposit(Dapp.AAVEV3, USDC, "400000000");
+      const usdcBalanceDelta = await balanceDelta(
+        pool.address,
+        USDC,
+        pool.signer
+      );
+      expect(usdcBalanceDelta.eq("400000000")).toBe(true);
+    });
   });
+};
 
-  it("approves unlimited USDC on Aave Lending pool", async () => {
-    await pool.approve(Dapp.AAVEV3, USDC, MAX_AMOUNT);
-    const usdcAllowanceDelta = await allowanceDelta(
-      pool.address,
-      USDC,
-      routerAddress[network]["aavev3"]!,
-      pool.signer
-    );
-    expect(usdcAllowanceDelta.gt(0)).toBe(true);
-  });
-
-  it("approves unlimited WETH on Aave Lending pool", async () => {
-    await pool.approve(Dapp.AAVEV3, WETH, MAX_AMOUNT);
-    const wethAllowanceDelta = await allowanceDelta(
-      pool.address,
-      USDC,
-      routerAddress[network]["aavev3"]!,
-      pool.signer
-    );
-    expect(wethAllowanceDelta.gt(0)).toBe(true);
-  });
-
-  it("lends 5 USDC into Aave lending pool", async () => {
-    await pool.lend(Dapp.AAVEV3, USDC, "5000000");
-    const usdcBalanceDelta = await balanceDelta(
-      pool.address,
-      USDC,
-      pool.signer
-    );
-    expect(usdcBalanceDelta.eq("-5000000"));
-  });
-
-  it("it borrows 0.001 WETH from Aave lending pool", async () => {
-    await pool.borrow(Dapp.AAVEV3, WETH, "1000000000000000");
-    const wethBalanceDelta = await balanceDelta(
-      pool.address,
-      WETH,
-      pool.signer
-    );
-    expect(wethBalanceDelta.eq("1000000000000000"));
-  });
-
-  it("it repays 0.001 WETH to Aave lending pool", async () => {
-    await pool.repay(Dapp.AAVEV3, WETH, "1000000000000000");
-    const wethBalanceDelta = await balanceDelta(
-      pool.address,
-      WETH,
-      pool.signer
-    );
-    expect(wethBalanceDelta.eq("-1000000000000000"));
-  });
-
-  it("it withdraws 4 USDC from Aave lending pool", async () => {
-    await pool.withdrawDeposit(Dapp.AAVEV3, USDC, "4000000");
-    const usdcBalanceDelta = await balanceDelta(
-      pool.address,
-      WETH,
-      pool.signer
-    );
-    expect(usdcBalanceDelta.eq("4000000"));
-  });
+testingHelper({
+  network: Network.ARBITRUM,
+  testingRun: testAave
 });
